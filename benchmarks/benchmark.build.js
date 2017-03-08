ae102036ce6a485c995d4d69d2b2f0f357a5256b
brianxautumn <shred@brianautumn.com>
2017-03-07 23:06:09 -0800

    removed old stuff, added npmignore

--- benchmarks/benchmark.build.js
+++ benchmarks/benchmark.build.js
@@ -76,14 +76,6 @@
 
 	var decoder = new jsvpx();
 
-	//get first test vector
-	var vectorFile = testVectors[0];
-	var testVectorRequest = new XMLHttpRequest();
-	testVectorRequest.open("GET", vectorFolder + vectorFile, true);
-	testVectorRequest.responseType = "arraybuffer";
-
-	console.warn(testVectors.length);
-
 
 	document.getElementById('run').onclick = function () {
 	    console.log("Starting Experiments");
@@ -101,7 +93,7 @@
 	};
 
 	function loadVector(vectorNumber) {
-	    //console.warn("Loading vector : " + vectorNumber);
+
 	    if (vectorNumber === testVectors.length) {
 	        runExperiment();
 	        return;
@@ -152,53 +144,13 @@
 
 	            });
 	        });
-	        
+
 	        console.warn(this);
-	        if(benchTest1.aborted)
+	        if (benchTest1.aborted)
 	            console.warn(benchTest1.error.stack);
 
 	    }).run();
 	}
-
-	testVectorRequest.onload = function (event) {
-
-	    var demuxer = new ivf();
-
-	    demuxer.receiveBuffer(testVectorRequest.response);
-	    demuxer.parseHeader();
-
-	    for (var i = 0; i < demuxer.frameCount; i++) {
-	        compressedFrames.push(demuxer.processFrame());
-	    }
-
-	    addTest(compressedFrames, vectorFile);
-	    /*
-	     suite.on('complete', function () {
-	     var benchTest1 = this[0];
-	     document.getElementById('output').innerHTML = JSON.stringify(benchTest1.stats , null , ' ');   
-	     }).run();
-	     */
-	};
-	//testVectorRequest.send(null);
-
-
-	function addTest(compressedFrames, file) {
-
-	    suite.add(file, function () {
-	        var decoder = new jsvpx();
-	        for (var i = 0; i < compressedFrames.length; i++) {
-	            decoder.decode(compressedFrames[i]);
-	        }
-
-	    });
-	}
-
-
-
-
-
-
-
 
 
 /***/ },
--- benchmarks/benchmark.js
+++ benchmarks/benchmark.js
@@ -23,14 +23,6 @@ var comprehensiveTestFrames = [];
 
 var decoder = new jsvpx();
 
-//get first test vector
-var vectorFile = testVectors[0];
-var testVectorRequest = new XMLHttpRequest();
-testVectorRequest.open("GET", vectorFolder + vectorFile, true);
-testVectorRequest.responseType = "arraybuffer";
-
-console.warn(testVectors.length);
-
 
 document.getElementById('run').onclick = function () {
     console.log("Starting Experiments");
@@ -48,7 +40,7 @@ document.getElementById('run').onclick = function () {
 };
 
 function loadVector(vectorNumber) {
-    //console.warn("Loading vector : " + vectorNumber);
+
     if (vectorNumber === testVectors.length) {
         runExperiment();
         return;
@@ -99,50 +91,10 @@ function runExperiment() {
 
             });
         });
-        
+
         console.warn(this);
-        if(benchTest1.aborted)
+        if (benchTest1.aborted)
             console.warn(benchTest1.error.stack);
 
     }).run();
 }
-
-testVectorRequest.onload = function (event) {
-
-    var demuxer = new ivf();
-
-    demuxer.receiveBuffer(testVectorRequest.response);
-    demuxer.parseHeader();
-
-    for (var i = 0; i < demuxer.frameCount; i++) {
-        compressedFrames.push(demuxer.processFrame());
-    }
-
-    addTest(compressedFrames, vectorFile);
-    /*
-     suite.on('complete', function () {
-     var benchTest1 = this[0];
-     document.getElementById('output').innerHTML = JSON.stringify(benchTest1.stats , null , ' ');   
-     }).run();
-     */
-};
-//testVectorRequest.send(null);
-
-
-function addTest(compressedFrames, file) {
-
-    suite.add(file, function () {
-        var decoder = new jsvpx();
-        for (var i = 0; i < compressedFrames.length; i++) {
-            decoder.decode(compressedFrames[i]);
-        }
-
-    });
-}
-
-
-
-
-
-
-
